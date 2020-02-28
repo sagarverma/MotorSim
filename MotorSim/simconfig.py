ed4dae0f507847a2b14433dfdc98fbd2d02ec7ba
Sagar <sagar15056@iiitd.ac.in>
2020-02-28 16:39:27 +0100

    simulator params description

--- MotorSim/simconfig.py
+++ MotorSim/simconfig.py
@@ -4,25 +4,25 @@ class SimConfig(object):
         Psdnom = 0.95
         Prdnom = 1
 
-        self.Data_Ts = 1/4000
+        self.Data_Ts = 1/4000           #Data aquisition rate
 
-        self.Ts = 0.000250
+        self.Ts = 0.000250              #Simulation rate
         self.Tpwm = 0.000250
         self.Tfixe = 500e-6
 
-        self.Rs = 1.5
-        self.Rr = 0.9
+        self.Rs = 1.5                   #Stator resistance
+        self.Rr = 0.9                   #Rorot resistance
 
-        self.Ls = 0.160
-        self.Lr = 0.160
-        self.Lm = 0.153
+        self.Ls = 0.160                 #Stator inductance
+        self.Lr = 0.160                 #Rotor inductance
+        self.Lm = 0.153                 #Mutual inductance
         self.Lfs = 0.007
         self.Lfr = 0.007
         self.Lmt = 0.210
 
         self.P1 = 0.7
         self.P2 = 1.5
-        self.np = 2
+        self.np = 2                     #Number of pole pairs
         self.Psdnom = Psdnom
 
         self.Uo = 15
@@ -37,12 +37,12 @@ class SimConfig(object):
         self.Vnom = 400
         self.SFC_Coeff = 100
 
-        self.J = 0.045
+        self.J = 0.045                  #Motor shaft inertia
         self.wrinit = 2*3.14*0
         self.thrinit = 0
         self.Jm = 0.045
 
-        self.tauL = 25
+        self.tauL = 25                  #Torque load 
         self.wL = 2*3.14*0
 
         self.wCurr = 0
