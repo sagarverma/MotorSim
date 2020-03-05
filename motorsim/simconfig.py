fb4825aeca847936e3e589a1269c7dede0d48bda
Sagar Verma <sagar15056@iiitd.ac.in>
2020-03-05 11:49:07 +0100

    parameters explanation added

--- motorsim/simconfig.py
+++ motorsim/simconfig.py
@@ -6,9 +6,9 @@ class SimConfig(object):
 
         self.Data_Ts = 1/4000           #Data aquisition rate
 
-        self.Ts = 0.000250              #Simulation rate
-        self.Tpwm = 0.000250
-        self.Tfixe = 500e-6
+        self.Ts = 0.000250              #Simulation time of the electrical part of the control law
+        self.Tpwm = 0.000250            #Simulation time of the electrical part of the control law
+        self.Tfixe = 500e-6             #Sampling time of the mechanical part of the control law
 
         self.Rs = 1.5                   #Stator resistance
         self.Rr = 0.9                   #Rorot resistance
@@ -16,44 +16,44 @@ class SimConfig(object):
         self.Ls = 0.160                 #Stator inductance
         self.Lr = 0.160                 #Rotor inductance
         self.Lm = 0.153                 #Mutual inductance
-        self.Lfs = 0.007
-        self.Lfr = 0.007
-        self.Lmt = 0.210
+        self.Lfs = 0.007                #Stator leakage inductance
+        self.Lfr = 0.007                #Rotor leakage inductance
+        self.Lmt = 0.210                #Parameter for magnetic saturation model
 
-        self.P1 = 0.7
-        self.P2 = 1.5
+        self.P1 = 0.7                   #Parameter for magnetic saturation model
+        self.P2 = 1.5                   #Parameter for magnetic saturation model
         self.np = 2                     #Number of pole pairs
-        self.Psdnom = Psdnom
+        self.Psdnom = Psdnom            #Nominal stator flux on axis-d
 
-        self.Uo = 15
-        self.Ulim = 415*(2/3)**0.5
+        self.Uo = 15                    #Not being used
+        self.Ulim = 415*(2/3)**0.5      #Voltage limitation, hard-coded directly in the simulink
 
-        self.Psinit = [ Psdnom,  0 ]
-        self.Prinit = [ Prdnom,  0 ]
-        self.Inom = 9.1
-        self.Prdnom = Prdnom
+        self.Psinit = [ Psdnom,  0 ]    #Initial value of the stator flux
+        self.Prinit = [ Prdnom,  0 ]    #Initial value of the rotor flux
+        self.Inom = 9.1                 #Nominal current
+        self.Prdnom = Prdnom            #Nominal rotor flux on axis-d
 
-        self.SLP_Coeff = 100
-        self.Vnom = 400
-        self.SFC_Coeff = 100
+        self.SLP_Coeff = 100            #Parameter of the control law
+        self.Vnom = 400                 #Nominal voltage
+        self.SFC_Coeff = 100            #Parameter of the control law
 
         self.J = 0.045                  #Motor shaft inertia
-        self.wrinit = 2*3.14*0
-        self.thrinit = 0
-        self.Jm = 0.045
+        self.wrinit = 2*3.14*0          #Initial value of rotor speed
+        self.thrinit = 0                #Initial value of the motor angle
+        self.Jm = 0.045                 #Inertia of the motor
 
-        self.tauL = 25                  #Torque load 
-        self.wL = 2*3.14*0
+        self.tauL = 25                  #Torque load
+        self.wL = 2*3.14*0              #Speed of the load
 
-        self.wCurr = 0
-        self.wSpeed_PI = 2*3.14*2.5
-        self.xiSpeed_PI = 1
-        self.wCurr_PI = 2*3.14*50
-        self.xiCurr_PI = 1/2
-        self.wSpeedEst = 2*3.14*500
+        self.wCurr = 0                  #Parameter of the control law
+        self.wSpeed_PI = 2*3.14*2.5     #Parameter of the control law
+        self.xiSpeed_PI = 1             #Parameter of the control law
+        self.wCurr_PI = 2*3.14*50       #Parameter of the control law
+        self.xiCurr_PI = 1/2            #Parameter of the control law
+        self.wSpeedEst = 2*3.14*500     #Parameter of the control law
 
-        self.wn = 2*3.14*50
-        self.Tn = 25
+        self.wn = 2*3.14*50             #Nominal speed (Not used)
+        self.Tn = 25                    #Nominal torque
 
     def get_config_json(self):
         return {key:value for key, value in self.__dict__.items() if not key.startswith('__') and not callable(key)}
